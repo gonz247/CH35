50534f30d1bedf60b36e95440bfb1bb2273365e9
gonz247 <pinamendozag@gmail.com>
2023-12-11 01:52:47 -0600

    Codigo de clase Javascript con Fer

---
+++ TareasFer/Javascript/script.js
@@ -0,0 +1,58 @@
+//comentarios de una sola linea
+
+/*comentario 
+multi
+linea*/
+
+// alert("hola mundo");
+
+
+// console.log('Este es un mensaje para mi consola');
+
+
+// var nombre = prompt('Ingresa tu nombre');
+// alert('Hola, ' + nombre + ' Bienvenido a la CH35');
+
+
+// var numero1= parseInt(prompt('Ingresa el primer numero:'));
+// var numero2= parseInt(prompt('Ingresa el segundo numero:'));
+
+
+// console.log("suma:" + (numero1 + numero2)  );
+// console.log('resta:' + (numero1 - numero2));
+// console.log('division:' + (numero1/numero2) );
+// console.log('multiplicacion:' + (numero1*numero2));
+
+// var edad= parseInt(prompt('Ingresa tu edad:'));
+// if(edad <=17) {
+//     console.log('estas chavo, joven')
+//     alert('estas chavo joven')}
+// else if(edad>18 & edad<=64){
+//     alert('Ya eres todo un adultx')
+//     console.log('Ya eres todo un adultx y te sabes la de chambear')}
+// else{
+//     alert('Descanse en paz chavo')
+//     console.log('Descanse en paz chavo')}
+// ;
+ 
+
+// TIPOS DE DATOS Y VARIABLES
+
+// scope es el alcance de neustra variable
+/*var, let, const la diferencia es el alcance:
+var tiene un alcance global
+let tiene un alcance local
+const se mantiene sin ningun cambio*/
+
+/*string:cadena de texto (lleva comillas)
+number:numero
+bollean: true/false
+null:nulo
+undefined: noesta asignado o definido*/
+
+let nombre="Daniel";
+let invitadosExtra= 2;
+let esMayorEdad = true;
+let edad= 25;
+let invitadoEspecial = null;
+let horaSalida = undefined;
